db8d405281613fbd114a46a52897a729c6a6bb8b
Javed <50339361+JavedHussain-06@users.noreply.github.com>
2024-02-26 03:30:17 +0530

    added data folder

--- Controllers/tourController.js
+++ Controllers/tourController.js
@@ -1,8 +1,46 @@
-const fs = require("fs");
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-  );
-  
+// const fs = require("fs");
+// const tours = JSON.parse(
+//     fs.readFileSync(`${__dirname}/../../dev-data/data/tours-simple.json`)
+//   );
+
+const fs = require('fs');
+
+// Function to read a file asynchronously
+function readFile(filePath, callback) {
+    // Open the file
+    fs.open(filePath, 'r', (err, fd) => {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        // Read data from the file
+        fs.readFile(fd, { encoding: 'utf8' }, (readErr, data) => {
+            // Close the file handle
+            fs.close(fd, (closeErr) => {
+                if (closeErr) {
+                    console.error('Error closing file:', closeErr);
+                }
+                
+                if (readErr) {
+                    callback(readErr);
+                } else {
+                    callback(null, data);
+                }
+            });
+        });
+    });
+}
+
+// Example usage
+readFile('../dev-data/data/tours.json', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+    } else {
+        console.log('File content:', data);
+    }
+});
+
   const getAllTours = (req, res) => {
     res.status(200).json({
       status: "success",
